f26f8ed79c8bc1a954b70bba4436456b1c2b161b
Hughes <hughestotimee@gmail.com>
2024-12-06 12:44:47 +0000

    jj

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 
 // Route File Import
-import { auth } from "./routes/auth";
+import { auth } from "./routes/auth.ts";
 import { limiter } from "./controller/limiter";
 import loggingMiddleware from "./middleware/logging";
 import startServer from "./controller/server";
